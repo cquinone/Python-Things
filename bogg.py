d2cb504e93eef5beb2cff69a4ac370a86e6a4119
cquinone <cquinone@andrew.cmu.edu>
2017-11-27 04:26:25 -0500

    Add files via upload

--- bogg.py
+++ bogg.py
@@ -1,11 +1,26 @@
-#first make grid that can pick a random assortment of letters so it matches 
-# asolid distribution of what works well -- start with just A-Z, 1/26 chance on each tile
-
 from Tkinter import *
 import numpy as np
 import random
-import string
 import enchant
+
+#function to make new baord when new games tarts after first game
+def newBoard(data):
+	N = data.len
+	if N == 5:
+		dice = ["AAAFRS","AAEEEE","AAFIRS","ADENNN","AEEEEM","AEEGMU","AEGMNN","AFIRSY","BJKQXZ","CCENST","CEIILT","CEILPT","CEIPST","DDHNOT","DHHLOR","DHLNOR","DHLNOR","EIIITT","EMOTTT","ENSSSU","FIPRSY","GORRWV","IPRRRY","NOOTUW","OOOTTU"]
+	if N == 4:
+		dice = ["AAEEGN","ABBJOO","ACHOPS","AFFKPS","AOOTTW","CIMOTU","DEILRX","DELRVY","DISTTY","EEGHNW","EEINSU","EHRTVW","EIOSST","ELRTTY","HIMNUQu","HLNNRZ"]
+
+	init_b = np.chararray((N,N))
+	for i in range(0,N):
+		for j in range(0,N):
+			die = random.choice(dice)
+			dice.remove(die)
+			letter = random.choice(die) #0:26 to cut off capital letters, these could clash later with checking words
+			init_b[i,j] = letter
+
+	print(init_b)
+	return init_b
 
 def match(data, x, y):
 	x = int((x-12)/40) #sub xinit jump and divide by spacing
@@ -13,10 +28,10 @@ def match(data, x, y):
 	#print "realx, realy: ", realx, realy
 	#print x,y, " letter: ", data.grid[x,y]
 	#print ""
-	if x < 0 or x > data.len:
+	if x < 0 or x >= data.len:
 		#write to board that you cant pick there, redraw -> going to drawboard brings you where? back?
 		return None
-	if y < 0 or y > data.len:
+	if y < 0 or y >= data.len:
 		return None
 	else:
 		return data.grid[x,y]
@@ -24,16 +39,16 @@ def match(data, x, y):
 
 def keyPressed(event, data, canvas):
 	if event.char == "l":
-		print "words found: "
-		print data.found
-		print ""
+		print("words found: ")
+		print(data.found)
+		print("")
 
 	if data.clicks != 1:
 		#dont do any directional stuff
 		pass
 	else:
 		#do dir stuff here
-		#check data curr place, draw, update data curr place
+		#check data current place, draw a line to new spot, update data curr place, add letter on new spot
 		# --- canvas create line to new place depending on key press, update crr place
 		if event.char == "a":
 			canvas.create_line(data.curr_pos[0],data.curr_pos[1], data.curr_pos[0]-40, data.curr_pos[1])
@@ -88,14 +103,6 @@ def keyPressed(event, data, canvas):
 			if letter != None:
 				data.curr_word = data.curr_word + letter
 
-
-		#if event.char == "e"
-		#	pass
-		#if event.char == "z"
-		#	pass
-		#if event.char == "c"
-		#	pass
-
 	return event.char
 
 def drawboard(canvas, data):
@@ -110,12 +117,12 @@ def drawboard(canvas, data):
 			canvas.create_line(x_init + i*(spacing),y_init,x_init+ i*(spacing),spacing*data.len + 12)
 			canvas.create_line(x_init, y_init + j*(spacing), spacing*data.len + 12, y_init + j*(spacing))
 			
-	#manually dd final lines on outside of board
+	#manually add final lines on outside of board
 	canvas.create_line(x_init + (data.len)*(spacing) ,y_init,x_init+ (data.len)*(spacing),spacing*data.len + 12)
 	canvas.create_line(x_init, y_init + data.len*(spacing), spacing*data.len + 12, y_init + data.len*(spacing))
+	canvas.create_text(300,50, text="Time left: ", font=font)
 
 
-	#canvas.create_line(3,12,3,spacing*data.len+12)
 
 
 
@@ -127,14 +134,19 @@ def word_start(canvas, event, data):
 	if data.clicks == 2:
 		#now the word has ended, reset and check, print if good
 		data.clicks = 0
-		if data.d.check(data.curr_word):
-			#add to list, print?
+		if data.d.check(data.curr_word) or (data.d.check(data.curr_word[0:1]+"U"+data.curr_word[1:]) and data.curr_word[0:1] == "Q"):
+			#add to list if long enough
+			if len(data.curr_word) <= 2:
+				#print "word too short"
+				data.curr_word = ""
+				return None, 0,0
+
 			data.found.append(data.curr_word)
-			print "adding this word: ", data.curr_word
+			#print "adding this word: ", data.curr_word
 			data.curr_word = ""
 		else:
 			#print that this word sux, not good.
-			print "word is invalid, nah"
+			#print "word is invalid, nah"
 			data.curr_word = ""
 		return None,0,0
 
@@ -143,34 +155,90 @@ def word_start(canvas, event, data):
 		data.curr_word = data.curr_word + letter
 	data.curr_pos[0] = x
 	data.curr_pos[1] = y
-	print data.curr_word
+	#print data.curr_word
 
 	
 	return letter, event.x,event.y
 	#	#also get rid of everything, so call drawboard?
 	#	drawboard(canvas,data)
 
+
+def score(data,canvas):
+	font = "Arial 10 bold"
+	score = 0
+	compiled = ""
+	once = 1
+	#check for doubles
+	check = []
+	#save them in here
+	doubled = []
+	for word in data.found:
+		if word in check:
+			print("doubled: ", word)
+			print("in:", check) 
+			doubled.append(word)
+			continue
+		#use big boggle scoring	
+		if len(word) == 4 or len(word) == 3:
+			score = score + 1
+		if len(word) == 5:
+			score = score + 2
+		if len(word) == 6:
+			score = score + 3
+		if len(word) == 7:
+			score = score + 5
+		if len(word) >= 8:
+			score = score + 11
+		compiled = compiled + word + ", "
+		check.append(word)
+
+	#add newlines where the wordbank would get too long	
+	for i in range(len(compiled)):
+		if i%45 == 0 and i != 0:
+			compiled = compiled[:i] + "\n" + compiled[i:]
+	#remove doubles
+	for word in doubled:
+		data.found.remove(word)
+
+
+	#add words found
+	canvas.create_text(155,368, text=compiled, font=font)
+	font = "Arial 12 bold"
+	#add score
+	canvas.create_text(295,80, text="Score: "+str(score), font=font)
+	canvas.create_text(65,330, text="FOUND WORDS: ", font=font)
+
 def timerFired(data,canvas):
+	font = "Arial 12 bold"
 	if data.timer == 0:
 		#end game somehow?
 		#or print "OVER"?
 		font = "Arial 34 bold"
-		canvas.create_text(int(data.len/2)+130, int(data.len/2)+140, text="GAME OVER!", font=font)
-		# reset everything
+		canvas.create_text(120, 242, text="GAME OVER!", font=font)
+		data.curr_word = ""
+		data.timer = 81
+		data.pause = True
+		data.grid = newBoard(data)
+		score(data,canvas)
+		data.found = []
+
 	else:
-		print "one second"
+		#print "one second, pause is: ", data.pause
 		data.timer = data.timer - 1
-
-
-
-
-
+		if data.timer != 81:
+			canvas.delete("curr")
+		canvas.create_text(350,50, text=str(data.timer), font=font, tag="curr")
+		#canvas.create_text(350,50, text="|||||||||||", font=font, fill="white")
+		
 def run():
 	def timerFiredWrapper(canvas, data):
-		print "firing timer"
+		#print "firing timer"
 		timerFired(data,canvas)
 		# pause, then call timerFired again
-		canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+		if data.pause == False:
+			canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+		else: 
+			print("paused...")
 
 	def drawboardWrapper(canvas, data):
 		canvas.delete(ALL)
@@ -179,25 +247,32 @@ def run():
 
 	def mousePressedWrapper(event, canvas, data):
 		#mousePressed(event, data)
-		letter,x,y = word_start(canvas, event, data)
-		if event.y < 0:
-			data.clicks = data.clicks - 1
-		if data.curr_word == "" and event.y >= 0:
-			drawboardWrapper(canvas, data)
+		if data.pause == False:
+			#print "data.pause is: ", data.pause		
+			letter,x,y = word_start(canvas, event, data)
+			if event.y < 0:
+				data.clicks = data.clicks - 1
+			if data.curr_word == "" and event.y >= 0:
+				drawboardWrapper(canvas, data)
 
 
 	def keyPressedWrapper(event, canvas, data):
-		if data.timer == 10:
+		if data.timer == 2*60:
 			timerFiredWrapper(canvas, data)
 		key = keyPressed(event, data, canvas)
 		if key == "n":
+			data.pause = False
 			drawboardWrapper(canvas, data)
+			timerFiredWrapper(canvas, data)
 	
-
-	dice = ["AAAFRS","AAEEEE","AAFIRS","ADENNN","AEEEEM","AEEGMU","AEGMNN","AFIRSY","BJKQXZ","CCENST","CEIILT","CEILPT","CEIPST","DDHNOT","DHHLOR","DHLNOR","DHLNOR","EIIITT","EMOTTT","ENSSSU","FIPRSY","GORRWV","IPRRRY","NOOTUW","OOOTTU"]
-	alphabet = string.letters
-	N = 5
 	np.set_printoptions(precision=3,linewidth=200, suppress=True)
+	size = raw_input("what size: ")
+	if size == "big" or size == "5":
+		N = 5	
+		dice = ["AAAFRS","AAEEEE","AAFIRS","ADENNN","AEEEEM","AEEGMU","AEGMNN","AFIRSY","BJKQXZ","CCENST","CEIILT","CEILPT","CEIPST","DDHNOT","DHHLOR","DHLNOR","DHLNOR","EIIITT","EMOTTT","ENSSSU","FIPRSY","GORRWV","IPRRRY","NOOTUW","OOOTTU"]
+	if size == "small" or size == "4":
+		N = 4
+		dice = ["AAEEGN","ABBJOO","ACHOPS","AFFKPS","AOOTTW","CIMOTU","DEILRX","DELRVY","DISTTY","EEGHNW","EEINSU","EHRTVW","EIOSST","ELRTTY","HIMNUQu","HLNNRZ"]
 	init_b = np.chararray((N,N))
 	for i in range(0,N):
 		for j in range(0,N):
@@ -206,8 +281,8 @@ def run():
 			letter = random.choice(die) #0:26 to cut off capital letters, these could clash later with checking words
 			init_b[i,j] = letter
 
-	print init_b
-	print ""
+	print(init_b)
+	print("")
 	# Set up data and call init
 	class Struct(object): pass
 	data = Struct()
@@ -218,10 +293,13 @@ def run():
 	data.curr_word = ""
 	data.found = [] #list of found words
 	data.d = enchant.Dict("en_US") #dictionary to check words in
-	data.timer = 1000 #seconds
+	data.timer = 81 #3 minutes in seconds
 	data.timerDelay = 1000 #milliseconds
+	data.pause = False #pause game
 	# create the root and the canvas
 	root = Tk()
+	root.lift()
+	root.attributes("-topmost", True)
 	canvas = Canvas(root, width=500, height=500)
 	canvas.pack()
 	# set up events
@@ -231,6 +309,8 @@ def run():
 	                        keyPressedWrapper(event, canvas, data))
 	# and launch the app
 	root.mainloop()  # blocks until window is closed
+	root.after(1, lambda: root.focus_force())
+	root.update()
 	print("bye!")
 
 run()
